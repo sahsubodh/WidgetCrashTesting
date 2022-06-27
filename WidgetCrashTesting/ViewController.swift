36af0e2534953d0693afd80e814b5977d5fa2f1a
Subodh Sah <ssah@clari.com>
2022-06-27 15:57:53 -0700

    Initial Commit

---
+++ WidgetCrashTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetCrashTesting
+//
+//  Created by Subodh Sah on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
